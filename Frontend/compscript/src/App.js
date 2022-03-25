fc6ed6afb2e40bf0d87be4e17692b7aec7a8a21a
WalterC <3064960620401@ingenieria.usac.edu.gt>
2022-03-24 19:07:32 -0600

    begin editor/interface

--- Frontend/compscript/src/App.js
+++ Frontend/compscript/src/App.js
@@ -1,11 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import Editor from "@monaco-editor/react";
 
 function App() {
   return (
-    <div className="App">
-      
+    <>
+    <div style={{ width: '90%', margin: 'auto' }}>
+      <Editor
+        className="texteditor"
+        height="400px"
+        theme="vs-dark"
+        defaultLanguage='typescript'
+        value=''
+        onChange={e => console.log(e)} />
     </div>
+    <div>Hrd</div>
+    
+    
+    </>
   );
 }
 
